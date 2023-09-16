0a05929959fd279148dc38b124d0352a8fb42d8b
AroNixBox <nixonbusinessmail@gmail.com>
2023-09-17 00:36:21 +0200

    ...

--- MiniJam3D/Assets/Scripts/CatView.cs
+++ MiniJam3D/Assets/Scripts/CatView.cs
@@ -1,148 +1,89 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class CatView : MonoBehaviour
 {
-    [Range(0f, 360f)]
-    public float range;
-    public float radius;
-    public float speed;
-    public float attackRange = 2.0f;  // Distance at which the zombie will attack the player
-    private Animator _animator;
-    [SerializeField] private float _attackDamage = 50f;
-    private float canAttack = 0f;
+    public float wanderRadius = 10f;
+    public float speed = 5f;
+    public float detectionRadius = 5f;
+    public float attackRange = 2.0f;
+    public float _attackDamage = 50f;
     
-
-    public GameObject Mouse;
-    private Rigidbody rb;
-
-    public LayerMask Target;
-    public LayerMask Obstruction;
-
-    public bool InView;
-
-    public Transform[] positions;
-    public int start;
-
-    private int i;
+    private Animator _animator;
+    private GameObject player;
+    private Vector3[] wanderPoints;
+    private int currentPoint = 0;
+    private bool isChasingPlayer = false;
+    private float canAttack = 0f;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
-        Mouse = GameObject.FindGameObjectWithTag("Player");
-        rb = GetComponent<Rigidbody>();
-        StartCoroutine(FOVRoutine());
-
-        transform.position = positions[start].position;
+        player = GameObject.FindGameObjectWithTag("Player");
+        wanderPoints = GenerateWanderPoints();
+        //transform.position = wanderPoints[0];
     }
+
+    private Vector3[] GenerateWanderPoints()
+    {
+        Vector3[] points = new Vector3[3];
+        for (int i = 0; i < 3; i++)
+        {
+            points[i] = (Random.insideUnitSphere * wanderRadius) + transform.position;
+            points[i].y = transform.position.y;
+        }
+        return points;
+    }
+
     private void Update()
     {
-        if (InView)
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        
+        if (distanceToPlayer <= detectionRadius && !isChasingPlayer)
         {
-            MoveTowardsPlayer();
+            isChasingPlayer = true;
+        }
+        
+        if (isChasingPlayer)
+        {
+            MoveTowards(player.transform.position);
+
+            if (distanceToPlayer <= attackRange)
+            {
+                AttackPlayer();
+            }
         }
         else
         {
-            MoveToPosition();
-        }
-    }
-
-    private IEnumerator FOVRoutine()
-    {
-        float delay = 0.2f;
-        WaitForSeconds wait = new WaitForSeconds(delay);
-
-        while (true)
-        {
-            yield return wait;
-            FOVCheck();
-        }
-    }
-
-    private void FOVCheck()
-    {
-        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, Target);
-
-        if (rangeChecks.Length != 0)
-        {
-            Transform target = rangeChecks[0].transform;
-            Vector3 directionToTarget = (target.position - transform.position).normalized;
-
-            if (Vector3.Angle(transform.forward, directionToTarget) < range / 2)
+            MoveTowards(wanderPoints[currentPoint]);
+            if (Vector3.Distance(transform.position, wanderPoints[currentPoint]) < 0.1f)
             {
-                float distanceToTarget = Vector3.Distance(transform.position, target.position);
-
-                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, Obstruction))
-                {
-                    InView = true;
-
-                    Vector3 direction = (target.position - transform.position).normalized;
-                    direction.y = 0;  // ensure we only move horizontally
-                    transform.forward = direction;  // make the cat face the player
-                }
-                else
-                {
-                    InView = false;
-                }
-            }
-            else
-            {
-                InView = false;
+                currentPoint = (currentPoint + 1) % 3; // Loop through the points
             }
         }
-        else if (InView)
-        {
-            InView = false;
-        }
     }
 
-    private void MoveTowardsPlayer()
+    private void MoveTowards(Vector3 target)
     {
-        float distanceToPlayer = Vector3.Distance(Mouse.transform.position, transform.position);
-
-        // If the zombie is close enough, it will attack the player
-        if (distanceToPlayer <= attackRange)
-        {
-            AttackPlayer();
-            return;
-        }
-
-        Vector3 direction = (Mouse.transform.position - transform.position).normalized;
-        direction.y = 0;  // Ensure movement is only horizontal
+        Vector3 direction = (target - transform.position).normalized;
+        direction.y = 0;
         transform.position += direction * speed * Time.deltaTime;
+        transform.forward = direction;
     }
 
     private void AttackPlayer()
     {
         _animator.SetTrigger("isAttacking");
+        // You can add more attack logic here, for now, it just triggers the animation
     }
 
     public void CanAttackPlayer(float canAttackValue)
     {
         canAttack = canAttackValue;
     }
+
     public void RequestDoDamageToPlayer(IDamageable player)
     {
-        if(canAttack == 1f)
+        if (canAttack == 1f)
             player.TakeDamage(_attackDamage);
-    }
-
-    private void MoveToPosition()
-    {
-        Vector3 direction = (positions[i].position - transform.position).normalized;
-
-        if (Vector2.Distance(transform.position, positions[i].position) < 0.1f)
-        {
-            i++;
-
-            if (i >= positions.Length)
-            {
-                i = 0;
-            }
-        }
-
-        transform.position = Vector3.MoveTowards(transform.position, positions[i].position, speed * Time.deltaTime);
-        transform.forward = direction;
     }
 }
--- MiniJam3D/Assets/Scripts/EnemyHealth.cs
+++ MiniJam3D/Assets/Scripts/EnemyHealth.cs
@@ -7,11 +7,14 @@ public class EnemyHealth : MonoBehaviour, IDamageable
 {
     private float _currentHealth;
     [SerializeField] private float maxHealth;
+    [SerializeField] private Collider _collider;
     private Animator _animator;
+    private Rigidbody _rb;
     private void Start()
     {
         _animator = GetComponent<Animator>();
         _currentHealth = maxHealth;
+        _rb = GetComponent<Rigidbody>();
     }
 
     public void TakeDamage(float hitDamageAmount)
@@ -25,8 +28,10 @@ public class EnemyHealth : MonoBehaviour, IDamageable
 
     public void Die()
     {
+        _collider.enabled = false;
+        _rb.isKinematic = true;
         GetComponent<CatView>().enabled = false;
         _animator.SetBool("Die", true);
-        Destroy(gameObject, 1f);
+        Destroy(gameObject, 3f);
     }
 }
